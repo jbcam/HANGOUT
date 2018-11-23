e4f572aa9c9404e94e9271ffc272bdf16175b302
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2018-11-23 15:36:56 +0100

    hide cards when not available

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/js/bootstrap';
 import "bootstrap";
 import { getProximity } from "../geolocation";
+import { toggleStatus } from "../status";
 
 document.addEventListener("DOMContentLoaded", getProximity);
+document.addEventListener("DOMContentLoaded", toggleStatus);
 
-
+// toggleStatus;
