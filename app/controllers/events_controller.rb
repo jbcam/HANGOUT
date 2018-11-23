10b306fdf4843ec7bbd1cd1bec3420b87b0bed4e
kevin joya <kevin.joya@outlook.com>
2018-11-23 16:34:13 +0100

    event create done and added margin bottom on the container home

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,33 @@
 class EventsController < ApplicationController
-
   def show
     @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      flash[:notice] = "Event created"
+      redirect_to @event
+    else
+      flash[:warning] = "Oops! something when wrong, please try again"
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :address, :category_id, :starts_at, :ends_at)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/js/bootstrap';
 import "bootstrap";
 import { getFriendsProximity } from "../geolocation";
+import "../plugins/flatpickr";
 
 document.addEventListener("DOMContentLoaded", getFriendsProximity);
 
