11021a113236666c6fdbe06eb11ed3a3bc21a552
Jean-Baptiste Camaret <jbaptiste.camaret@gmail.com>
2018-11-26 09:52:31 +0100

    fix chat navbar implement single chat per user pair

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,8 +1,5 @@
 module ConversationsHelper
-  def display_time(user1, user2)
-
-    return start if event.ends_at.nil?
-
-    "#{start} - #{event.ends_at.strftime('%d %b %H:%M')}"
+  def conversation_partner(message)
+    message.sender == current_user ? message.recipient : message.sender
   end
 end
