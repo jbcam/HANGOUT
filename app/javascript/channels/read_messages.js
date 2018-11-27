5b180b197bc2f778ec9af3bad0444984aacc0322
Jean-Baptiste Camaret <jbaptiste.camaret@gmail.com>
2018-11-27 12:18:22 +0100

    ajax unread messages

--- app/javascript/channels/read_messages.js
+++ app/javascript/channels/read_messages.js
@@ -2,13 +2,14 @@ const updateReadMessages = () => {
   fetch(document.URL , {
     method: "PATCH",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      'X-CSRF-Token': Rails.csrfToken()
     }
   })
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data); // Look at local_names.default
-    });
+    // .then(response => response.json())
+    // .then((data) => {
+    //   console.log(data); // Look at local_names.default
+    // });
 };
 
 const input = document.getElementById("message_content");
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,4 +3,9 @@ class Conversation < ApplicationRecord
   belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
 
   has_many :messages, as: :messageable, dependent: :destroy
+
+  def unread_messages(user)
+    count = messages.count { |message| !message.read && message.user != user }
+    count.nil? ? 0 : count
+  end
 end
