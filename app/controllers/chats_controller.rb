c5d4707e64ea4bce8477bec96d1ef1f29a8f4799
Jean-Baptiste Camaret <jbaptiste.camaret@gmail.com>
2018-11-28 17:27:43 +0100

    fix chat bug

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,7 @@
 class ChatsController < ApplicationController
   def index
     @channels = current_user.events + current_user.recipient_conversations + current_user.sender_conversations
-    @channels.sort_by { |channel| channel.messages.last.created_at }
+
+    @channels.sort_by { |channel| channel.messages.last ? channel.messages.last.created_at : 0 }
   end
 end
