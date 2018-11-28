cfa44fae31ab76a7e7e81f73f380ca5a55f8163d
Jean-Baptiste Camaret <jbaptiste.camaret@gmail.com>
2018-11-28 21:01:53 +0100

    update app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/js/bootstrap';
 import "bootstrap";
 import "../plugins/flatpickr";
 import "../channels/chatrooms";
-import "../channels/messages";
 import "../channels/read_messages";
 import { getProximity } from "../geolocation";
 // import { toggleStatus } from "../status";
