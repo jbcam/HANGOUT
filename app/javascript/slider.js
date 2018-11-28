8d687c7689f02daf9737c3eca6913a52f70b036a
Nicolas Bejenne <nicolas.bejenne@gmail.com>
2018-11-28 16:40:38 +0100

    the back of location slider is ok

--- app/javascript/slider.js
+++ app/javascript/slider.js
@@ -6,7 +6,25 @@ function startSlide() {
   var mySlider = new Slider(input);
 
   mySlider.on('slideStop', function() {
-    console.log(mySlider.getValue())
+
+    document.querySelectorAll('.card-user').forEach((card) => {
+       var kmSelector = mySlider.getValue()
+       var distance = parseInt(card.querySelector('.card-km').innerHTML.match(/^\d*/)[0], 10)
+       console.log(distance)
+
+        if (distance < kmSelector) {
+          card.classList.remove('d-none');
+          console.log("AFFICHER")
+       console.log(distance)
+        } else {
+          card.classList.add('d-none');
+          console.log("REMOVE")
+       console.log(distance)
+        }
+      // addEventListener("click", (event) => {
+      //   event.currentTarget.classList.toggle("img-circle");
+      // });
+    });
   })
 }
 
